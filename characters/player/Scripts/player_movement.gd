0a5f913df58a870cc191982720369110ac94ce16
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2025-12-08 01:50:18 +0800

    feat: add base player

---
+++ characters/player/Scripts/player_movement.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@onready var movement = get_node("BaseMovement")
+
+@export var speed = 300.0
+@export var jump_strength = -400.0
+
+func _physics_process(delta: float) -> void:
+	movement.apply_gravity(delta)
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		movement.move_right(delta, speed)
+	elif direction < 0:
+		movement.move_left(delta, speed)
+	else:
+		movement.stop()
+		
+	if Input.is_action_pressed("jump"):
+		movement.jump(jump_strength)
+
+	move_and_slide()
