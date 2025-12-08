a730e2c86a3763f4a92390feb59d9b6fe0a88e22
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2025-12-08 16:52:38 +0800

    fix: movement on steroids

--- characters/Components/base_movement.gd
+++ characters/Components/base_movement.gd
@@ -3,8 +3,9 @@ extends Node
 @onready var parent = get_parent()
 @onready var hurtbox = parent.get_node("Hurtbox")
 
-var acceleration = 50.0
+var acceleration = 10.0
 var max_speed = 300.0
+
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
@@ -22,14 +23,16 @@ func _physics_process(delta: float) -> void:
 
 func apply_gravity(delta: float) -> void:
 	parent.velocity += parent.get_gravity() * delta
-
+	
 func move_left(delta: float, modifier: float) -> void:
 	if not parent.is_knocked_back:
-		parent.velocity.x = lerp(parent.velocity.x, -max_speed, acceleration * modifier * delta)
+		var target_speed = -max_speed * modifier
+		parent.velocity.x = lerp(parent.velocity.x, target_speed, acceleration * delta)
 
 func move_right(delta: float, modifier: float) -> void:
 	if not parent.is_knocked_back:
-		parent.velocity.x = lerp(parent.velocity.x, max_speed, acceleration * modifier * delta)
+		var target_speed = max_speed * modifier
+		parent.velocity.x = lerp(parent.velocity.x, target_speed, acceleration * delta)
 
 func jump(delta: float, jump_strength: float) -> void:
 	if parent.is_on_floor() and not parent.is_knocked_back:
